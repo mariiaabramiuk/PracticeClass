68e8b127b7721d12b613001294f122a3a9a88363
mariiazatylna <mariiazatylna@gmail.com>
2023-11-06 14:33:02 -0600

    Adding new Project

---
+++ src/test/java/PracticeClass.java
@@ -0,0 +1,8 @@
+public class PracticeClass {
+    public static void main(String[] args) {
+        System.out.println("Hello batch3");
+        String str = "Hello world";
+        System.out.println(str);
+
+    }
+}
